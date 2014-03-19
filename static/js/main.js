3a3b0280b579e420b413bda1dbf359dc8afcfd2a
NeilMakn <nmaknarakham@gmail.com>
2014-03-19 04:25:18 -0700

    lots of adds, updated pages, added photos, last things before deploy include fixing content

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 	console.log("Hello!");
 
+var image_tag = ''
 
+var i = 0;
+
+for(i=11; i < 44; i++) {
+  $('ul#loadimg').append('<li><a class="th" href="/static/img/' + [i] + '.jpg"><img src="/static/img/' + [i] + '.jpg"></a></li>');
+};
 
 });
