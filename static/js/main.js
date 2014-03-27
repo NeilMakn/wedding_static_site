46032d497f3565ca3f47fc555a7766690ca6be0c
NeilMakn <nmaknarakham@gmail.com>
2014-03-26 18:06:48 -0700

    removed unwanted photos

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,37 @@
 $(document).ready(function() {
 	console.log("Hello!");
 
-var image_tag = ''
 
-var i = 0;
 
-for(i=11; i < 44; i++) {
-  $('ul#loadimg').append('<li><a class="th" href="/static/img/' + [i] + '.jpg"><img src="/static/img/' + [i] + '.jpg"></a></li>');
+var engagement_photos = []
+for (i = 11; i <= 44; i++) {
+  engagement_photos.push(i);
+}
+
+//remove photos kendra doesn't like
+function remove_engag_photos(element) {
+  // badphotos = [13, 16, 17, 19];
+  return element != 13 && element != 16 && element != 17 && element != 19
 };
 
+// probably best to just delete photos and then re-number them.
+// remove_engag_photos function would then become unnecessary.
 
+var filt_engag_photos = engagement_photos.filter(remove_engag_photos);
+
+console.log(filt_engag_photos);
+
+// var i = 0;
+
+// for(i=11; i < 44; i++) {
+
+//     $('ul#loadimg').append('<li><a class="th" href="/static/img/' + [i] + '.jpg"><img src="/static/img/' + [i] + '.jpg"></a></li>');
+// };
+
+
+for (i = 0; i < filt_engag_photos.length; i++ ) {
+  $('ul#loadimg').append('<li><a class="th" href="/static/img/' + filt_engag_photos[i] + '.jpg"><img src="/static/img/' + filt_engag_photos[i] + '.jpg"></a></li>');
+
+}
 
 });
