082d9c523ea78ec0bb0db283403f4253e13ec899
NeilMakn <nmaknarakham@gmail.com>
2014-03-31 13:31:05 -0700

    all content updated, ready to be deployed prior to spell checks and final look over by the future misses

--- static/js/main.js
+++ static/js/main.js
@@ -21,12 +21,21 @@ var filt_engag_photos = engagement_photos.filter(remove_engag_photos);
 
 
 for (i = 0; i < filt_engag_photos.length; i++ ) {
-  $('ul#loadimg').append('<li><a class="th" href="/static/img/' + filt_engag_photos[i] + '.jpg"><img src="/static/img/' + filt_engag_photos[i] + '.jpg"></a></li>');
+  $('ul#photoset1').append('<li><a class="th" href="/static/img/' + filt_engag_photos[i] + '.jpg"><img src="/static/img/' + filt_engag_photos[i] + '.jpg"></a></li>');
+}
 
+// load 'years' album
+var years_album = []
+for (i = 1; i <= 9; i++) {
+  years_album.push(i);
+}
+console.log(years_album)
+for (i = 0; i < years_album.length; i++) {
+  $('ul#photoset2').append('<li><a class="th" href="/static/img/years/' + years_album[i] + '.jpg"><img src="/static/img/years/' + years_album[i] + '.jpg"></a></li>');
 }
 
 
-// fadein Load
+// fadein Load images
 
 
 $('img').hide();
